f8e3f0b7b9fc8dbef2c4a8f45bbc27937c13aea9
CaSSiON28 <pinkhanvananont@gmail.com>
2024-02-01 02:54:04 +0700

    File upload (copied)

---
+++ main.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, redirect, url_for
+app = Flask(_name_)
+app.secret_key='secret'
+
+if __name__ == '__main__':
+    app.run()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
